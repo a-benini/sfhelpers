cef36a571f775a7b79e70f4eb0d238a260675b48
Attilio Benini <attilio.benini@gmx.net>
2022-02-23 18:18:46 +0100

    add argument check_overlap to st_or()

--- R/st_or.R
+++ R/st_or.R
@@ -13,6 +13,10 @@
 #' arguments \code{x} and \code{y} according to \code{x.suffix}, resp.
 #' \code{y.suffix}; \code{FALSE} (default) suffixes only homonymous attribute
 #' headings.
+#' @param check_overlap \code{TRUE} detects those geometries included in
+#' \code{x} and \code{y} which overlap with the other input layer and applies
+#' geometric operations only on these; \code{FALSE} (default) applies geometric
+#' operations on all geometries (s. Details).
 #'
 #' @return geometry set containing the intersection of \code{x} and \code{y} and
 #' the non-overlapping parts of \code{x} and \code{y}. The attribute table is
@@ -34,11 +38,25 @@
 #'   corresponding to the geometry set they originated from (s. below examples)
 #'   \item handling of input layers with differently named geometry columns
 #'   and/or being totally overlapped by the other input layer.
+#'   \item optional restriction of the geometric operations to geometries of the
+#'   input layers \code{x} and \code{y} overlapping those of the other input
+#'   layer (argument \code{check_overlap = TRUE}). In case of a significant
+#'   proportion of non-overlapping geometries, this can shorten run time. As the
+#'   internal distinction of overlapping and non-overlapping geometries relies
+#'   on \code{\link[sf]{st_intersects}}, which itself is time consuming, setting
+#'   \code{check_overlap} to \code{TRUE} involves a trade off: If all geometries
+#'   overlap, \code{check_overlap = TRUE} will only increase the run time. On
+#'   the other hand, the higher the proportion of non-overlapping geometries,
+#'   the more this argument specification shortens the processing time by
+#'   leaving out unnecessary geometric operations. Ideally the user has previous
+#'   knowledge about how little or how much the input layers \code{x} and
+#'   \code{y} overlap with each other in order to apply \code{check_overlap} in
+#'   a informed way.
 #' }
 #'
 #' @importFrom data.table as.data.table rbindlist
 #' @importFrom sf st_crs st_sf st_agr st_dimension st_intersection st_geometry
-#' st_drop_geometry st_is st_sfc
+#' st_drop_geometry st_is st_sfc st_intersects
 #' @importFrom uuid UUIDgenerate
 #'
 #' @examples
@@ -101,7 +119,7 @@
 #'   st_or(A, B, dim = c(0, 1, 2)) # returns points, lines (& if available surfaces)
 #' )
 #' @export
-st_or <- function(x, y, dim = 2, x.suffix = ".x", y.suffix = ".y", suffix.all = FALSE) {
+st_or <- function(x, y, dim = 2, x.suffix = ".x", y.suffix = ".y", suffix.all = FALSE, check_overlap = FALSE) {
   # if x or y are not of the class "sf" or "sfc" throw a corresponding error message
   if (!any(c("sf", "sfc") %in% class(x))) {
     stop(
@@ -151,6 +169,10 @@ st_or <- function(x, y, dim = 2, x.suffix = ".x", y.suffix = ".y", suffix.all = 
     stop("suffix.all must be a single logical value: TRUE or FALSE", call. = FALSE)
   }
 
+  if(!isTRUE(check_overlap) & !isFALSE(check_overlap)){
+    stop("check_overlap must be a single logical value: TRUE or FALSE", call. = FALSE)
+  }
+
   # in case x or y are simple feature geometry list columns turn them into sf objects
   if ("sfc" %in% class(x)) {
     x <- sf::st_sf(geometry = x)
@@ -185,6 +207,19 @@ st_or <- function(x, y, dim = 2, x.suffix = ".x", y.suffix = ".y", suffix.all = 
   # to get the remainder of the intersection sfhelpers:::st_erase()* is used (s. below) instead of the formerly here coded st_erase()
   # (*improved version of st_erase() found under ?st_difference)
 
+  if(check_overlap){ # separate intersecting and non-intersecting geometries
+    int      <- sf::st_intersects(x, y)
+    int_x    <- lengths(int) > 0
+    int_y    <- seq_len(nrow(y)) %in% unique(unlist(int))
+    x_no_int <- x[!int_x, ]
+    x        <- x[int_x, ]
+    y_no_int <- y[!int_y, ]
+    y        <- y[int_y, ]
+  } else { # placeholder with zero rows
+    x_no_int <- x[NULL, ]
+    y_no_int <- y[NULL, ]
+  }
+
   # get overlap via intersection
   overlap <- sf::st_intersection(x, y)
 
@@ -193,7 +228,7 @@ st_or <- function(x, y, dim = 2, x.suffix = ".x", y.suffix = ".y", suffix.all = 
   y_diff <- st_erase(y, x)
 
   # stack the intersecting and non-intersecting parts and set missing attributes to NA
-  l                <- list(overlap, x_diff, y_diff)
+  l                <- list(overlap, x_diff, y_diff, x_no_int, y_no_int)
   geometry         <- do.call(c, lapply(l, sf::st_geometry))
   tmp_col          <- uuid::UUIDgenerate()
   get_non_geometry <- function(x) { data.table::as.data.table(sf::st_drop_geometry(x), keep.rownames = tmp_col) }
